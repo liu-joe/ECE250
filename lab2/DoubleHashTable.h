dcad5a07472abc7be53202fbaa206770d77eff06
Joe <z324liu@uwaterloo.ca>
2016-02-11 00:52:37 -0500

    Minor bug fixes and added comments

--- lab2/DoubleHashTable.h
+++ lab2/DoubleHashTable.h
@@ -2,9 +2,9 @@
 #define DOUBLE_HASH_TABLE_H
 
 /****************************************
- * UW User ID:  uwuserid
+ * UW User ID:  z324liu
  * Submitted for ECE 250
- * Semester of Submission:  (Winter|Spring|Fall) 20NN
+ * Semester of Submission:  Winter 2016
  *
  * By submitting this file, I affirm that
  * I am the author of all modifications to
@@ -63,52 +63,77 @@ DoubleHashTable<T >::~DoubleHashTable() {
     delete[] array_state;
 }
 
+/*
+ *  Return the number of elements in the hashtable
+ */
 template<typename T >
 int DoubleHashTable<T >::size() const {
     return count;
 }
 
+/*
+ *  Return the capacity of the hash table
+ */
 template<typename T >
 int DoubleHashTable<T >::capacity() const {
     return array_size;
 }
 
-
-
+/*
+ *  Checks if the array is empty, return true if empty, return false otherwise
+ */
 template<typename T >
 bool DoubleHashTable<T >::empty() const {
     return count == 0;
 }
 
+/*
+ *  Returns the result of the first hash function, always positive
+ */
 template<typename T >
 int DoubleHashTable<T >::h1( T const &obj ) const {
     return ((static_cast<int> (obj) % array_size) + array_size) % array_size;
 }
 
+/*
+ *  Returns the result of the second hash function, always positive
+ */
 template<typename T >
 int DoubleHashTable<T >::h2( T const &obj ) const {
     return (((static_cast<int> (obj) / array_size) % array_size) + array_size) 
             % array_size;
 }
 
+/*
+ *  Checks if the obj exists within the hashtable, return true if it exists,
+ *  and return false otherwise
+ */
 template<typename T >
 bool DoubleHashTable<T >::member( T const &obj ) const {
-    if ( count == array_size ) { return false; }
+    if ( count == 0 ) { return false; }
     
-    int i = 0;
+    
+    // Get values of first and second hash function, and make sure that the
+    // result of the second hash function is odd
     int h1key = h1(obj);
     int h2key = h2(obj) + (h2(obj) + 1) % 2;
+    int i = 0;
     
     for (int idx = 0; idx < array_size; ++idx) {
         if ( array[(h1key + idx * h2key) % array_size] == obj ) {
             if ( array_state[(h1key + idx * h2key) % array_size] == OCCUPIED) {
                 return true;
+            } else if (array_state[(h1key + idx * h2key) % array_size] == EMPTY) {
+                return false;
             }
         }
     }
     return false;
 }
 
+/*
+ *  Returns the content of the specified bin
+ */
 template<typename T >
 T DoubleHashTable<T >::bin( int n ) const {
     
@@ -119,19 +144,29 @@ T DoubleHashTable<T >::bin( int n ) const {
 
 }
 
+/*
+ *  Inserts the given object at index based on the results of the hash functions
+ */
 template<typename T >
 void DoubleHashTable<T >::insert( T const &obj ) {
     
-    if ( count == array_size ) { return ; }
+    if ( count == array_size ) { 
+        throw overflow();
+        return ; 
+    }
     
-    int i = 0;
+    // Get values of first and second hash function, and make sure that the
+    // result of the second hash function is odd
     int h1key = h1(obj);
     int h2key = h2(obj) + (h2(obj) + 1) % 2;
+    int i = 0;
     
     while (array_state[(h1key + i * h2key) % array_size] == OCCUPIED) {
+        if (array[(h1key + i * h2key) % array_size] == obj) {
+            return ;
+        }
         ++i;
     }
-    std::cout << i << std::endl;
     array[(h1key + i * h2key) % array_size] = obj;
     array_state[(h1key + i * h2key) % array_size] = OCCUPIED;
     ++count;
@@ -139,10 +174,16 @@ void DoubleHashTable<T >::insert( T const &obj ) {
     
 }
 
+/*
+ *  Removed the object from the hash table, return true if object removed,
+ *  return false of the object is not in the table
+ */
 template<typename T >
 bool DoubleHashTable<T >::remove( T const &obj ) {
     if ( count == 0 ) { return false; }
     
+    // Get values of first and second hash function, and make sure that the
+    // result of the second hash function is odd
     int h1key = h1(obj);
     int h2key = h2(obj) + (h2(obj) + 1) % 2;
     
@@ -161,6 +202,9 @@ bool DoubleHashTable<T >::remove( T const &obj ) {
     return false;
 }
 
+/*
+ *  Clear the table by reseting all the array states to empty
+ */
 template<typename T >
 void DoubleHashTable<T >::clear() {
     for ( int i = 0; i < array_size; ++i ) {
@@ -168,6 +212,9 @@ void DoubleHashTable<T >::clear() {
     }
 }
 
+/*
+ *  Prints out the content of the hashtable
+ */
 template<typename T >
 void DoubleHashTable<T >::print() const {
     for (int idx = 0; idx < array_size; ++idx) {
--- lab2/main.cpp
+++ lab2/main.cpp
@@ -1,12 +1,28 @@
 
+#include <iostream>
 #include "DoubleHashTable.h"
+#include "Exception.h"
 
 int main() {
     DoubleHashTable<int> ht;
-    ht.insert(123);
-    ht.insert(1543);
-    for (int idx = 1; idx < 31; ++idx) {
-        ht.insert(34 * idx);
+    try {
+         ht.insert(123);
+         ht.insert(1543);
+    } catch (overflow e) {
+        std::cout << "overflow" << std::endl;
+    }
+         
+    std::cout << ht.bin(1) << std::endl;
+    std::cout << ht.bin(2) << std::endl;
+    std::cout << ht.bin(3) << std::endl;
+    std::cout << ht.bin(4) << std::endl;
+    std::cout << ht.bin(5) << std::endl;
+    for (int idx = 1; idx < 35; ++idx) {
+        try {
+            ht.insert(34 * idx);
+        } catch (overflow e) {
+            std::cout << "overflow" << std::endl;
+        }
     }
     
     ht.print();
